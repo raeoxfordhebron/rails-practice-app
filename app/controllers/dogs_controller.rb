0df90eb1bb0ef176485894a7034d1ac6d690b004
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-28 15:01:12 -0400

    able to create a new dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,12 @@
 class DogsController < ApplicationController
     def index
     end
+    def show
+    end
     def create
+        @dog = Dog.new(params.require(:dogs).permit(:name, :age))
+        @dog.save
+        redirect_to @dog
     end
     def new
     end
